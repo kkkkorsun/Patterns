8650f15c4db331805f041b62c794e04ea515ddd3
kcvpll <korsun.marketing@gmail.com>
2019-10-05 14:24:21 +0300

    Factory pattern

---
+++ src/patterns/Factory.java
@@ -0,0 +1,37 @@
+package patterns;
+
+public class Factory {
+    public static void main(String[] args){
+        Factory factory = new Factory();
+        Car toyota = factory.create("Toyota");
+        Car audi = factory.create("Audi");
+        toyota.drive();
+        audi.drive();
+    }
+}
+
+interface Car{
+    void drive();
+}
+
+class Toyota implements Car{
+    @Override
+    public void drive(){
+        System.out.println("drive Toyota");
+    }
+}
+class Audi implements Car{
+    @Override
+    public void drive(){
+        System.out.println("drive Audi");
+    }
+}
+class FactoryL{
+    public Car create(String typeOfCar){
+        switch (typeOfCar){
+            case "Toyota" : return new Toyota();
+            case "Audi" : return new Audi();
+            default: return null;
+        }
+    }
+}
